5477c8f83397050d9b52bb749c4759ed2d80fda6
Absolute Value <heejun1708@gmail.com>
2025-11-19 10:43:51 +0900

    feat: add toDomain fun to model

--- app/src/main/java/com/tukorea/bus/data/model/TempListResponse.kt
+++ app/src/main/java/com/tukorea/bus/data/model/TempListResponse.kt
@@ -1,5 +1,9 @@
 package com.tukorea.bus.data.model
 
+import com.tukorea.bus.domain.model.Temp
+
 data class TempListResponse(
     val data: List<TempResponse>
-)
+) {
+    fun toDomain(): List<Temp> = data.flatMap { it.toDomain() }
+}
--- app/src/main/java/com/tukorea/bus/data/model/TempResponse.kt
+++ app/src/main/java/com/tukorea/bus/data/model/TempResponse.kt
@@ -1,5 +1,9 @@
 package com.tukorea.bus.data.model
 
+import com.tukorea.bus.domain.model.Temp
+
 data class TempResponse(
     val data: List<Temp>
-)
+) {
+    fun toDomain(): List<Temp> = data.map { Temp(it.temp) }
+}
