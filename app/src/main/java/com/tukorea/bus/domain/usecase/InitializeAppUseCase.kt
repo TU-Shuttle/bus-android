f5f8c52f05345d2f11b3ef8b874cbb9623b70277
김현민 <rlagusals1102@gmail.com>
2025-11-21 15:06:06 +0900

    feat: add InitializeAppUseCase class

---
+++ app/src/main/java/com/tukorea/bus/domain/usecase/InitializeAppUseCase.kt
@@ -0,0 +1,12 @@
+package com.tukorea.bus.domain.usecase
+
+import javax.inject.Inject
+
+class InitializeAppUseCase @Inject constructor() {
+    
+    suspend operator fun invoke() {
+        // TODO:  초기화 로직 추가
+
+    }
+}
+
